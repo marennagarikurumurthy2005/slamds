42b2944b7faf047fe58647cab44210dffe105ced
marennagarikurumurthy2005 <marennagarikurumurthy2005@gmail.com>
2026-04-07 20:14:04 +0530

    backend fix

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from config.views import WakeupView
 
 urlpatterns = [
+    path("", WakeupView.as_view(), name="root-wakeup"),
     path("admin/", admin.site.urls),
     path("api/v1/wakeup/", WakeupView.as_view(), name="wakeup"),
     path("api/v1/auth/", include("accounts.urls")),
